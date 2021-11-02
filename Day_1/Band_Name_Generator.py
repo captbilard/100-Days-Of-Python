d978c1762e6bbc938861dac93c04fcd90ac5713b
Adebayo Osinulu <bilardosinulu@gmail.com>
2021-11-02 22:14:43 +0100

    Day 1 Task

---
+++ Day_1/Band_Name_Generator.py
@@ -0,0 +1,10 @@
+import time
+
+print("Welcome to the band name generator")
+time.sleep(1)
+print("What city did you grow up in?")
+city = input()
+time.sleep(1)
+print("What is the name of your pet?")
+pet = input()
+print(f"Your band name is {city} {pet}.")
