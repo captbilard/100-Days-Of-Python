034d0752a27fd424b89cc2fa18366e9fb533fc63
Adebayo Osinulu <bilardosinulu@gmail.com>
2021-11-03 22:43:06 +0100

    Renamed all files to follow a sequential order

--- utils/create_file.py
+++ utils/create_file.py
@@ -1,2 +1,8 @@
-for x in range(2,101):
-    f = open(f'Day_{x}_Challenge.py', 'x')
+x = 3
+while (x<10):
+    open(f'../Day_00{x}_Challenge.py', 'x')
+    x += 1
+while (x<100):
+    open(f'../Day_0{x}_Challenge.py', 'x')
+    x += 1
+open(f'../Day_{x}_Challenge.py', 'x')
