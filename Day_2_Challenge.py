dcf5d8ad0ec29afb6579871b1b67009ab3ce485a
Adebayo Osinulu <bilardosinulu@gmail.com>
2021-11-03 22:29:25 +0100

    Completed Day 2 Day_2_Challenge

--- Day_2_Challenge.py
+++ Day_2_Challenge.py
@@ -1,1 +1,14 @@
-print("hello")
+# Your life in days, weeks, months if you live to 90 years
+
+# Get the users age
+age = input("What is your current age?\n")
+# Check input is valid
+if not age.isnumeric():
+    print("Enter a valid number from 0-90")
+elif int(age) > 90:
+    print("Enter a valid number from 0-90")
+
+age_difference = 90 - int(age)
+days, weeks, months = 365 * age_difference, 52 * age_difference, 12 * age_difference
+
+print(f"You have {days} days, {weeks} weeks and {months} months left.")
